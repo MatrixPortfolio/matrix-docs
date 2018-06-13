850a3d982ec7e12c2f9660dd4bf8a0862a6b0586
Mathew Chan <mail@mathewchan.com>
2018-06-13 01:57:13 -0400

    Adds initial query to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,34 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <div>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
+
+    <h2>Articles</h2>
+    {data.allMarkdownRemark.edges.map(doc => {
+      <a key={doc.node.id} href="{doc.node.frontmatter.path}">{doc.node.frontmatter.title}</a>
+    })}
   </div>
 )
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            path
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
